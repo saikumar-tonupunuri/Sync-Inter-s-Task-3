f36f96ba11e11af3070df7cbac1d6c64ae342b6e
saikumar-tonupunuri <117814724+saikumar-tonupunuri@users.noreply.github.com>
2023-04-03 23:44:04 +0530

    Add files via upload

---
+++ musicdata.js
@@ -0,0 +1,32 @@
+let songs = [
+    {
+      name: "Yemaiundacchoo",
+      artist: "Deepthi Sunaiana,Vinay Shanmukh",
+      img: "Image1",
+      audio: "Music1"
+    },
+    {
+      name: "Yedurangula Vaana",
+      artist: "Sid Sriram",
+      img: "Image2",
+      audio: "Music2"
+    },
+    {
+      name: "Naatu Naatu",
+      artist: "Rahul Sipligunj, Kaala Bhairava",
+      img: "Image3",
+      audio: "Music3"
+    },
+    {
+      name: "Dhoom Dhaam Dhosthaan",
+      artist: "Rahul Sipligunj,Gotte Kanakavva",
+      img: "Image4",
+      audio: "Music4"
+    },
+    {
+      name: "Chamkeela Angeelesi",
+      artist: "Ram Miriyala,Dhee",
+      img: "Image5",
+      audio: "Music5"
+    }
+  ]
